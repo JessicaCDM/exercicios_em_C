483c974731fc3ca920c1c100eed416c4a6e230f1
Jessica Miranda <jessica.donzelli@gmail.com>
2022-09-28 13:39:41 +0100

    curso_CET

---
+++ mensagem.h
@@ -0,0 +1,10 @@
+#ifndef D43627EB_42A8_48A7_A9CD_7D306B38AE53
+#define D43627EB_42A8_48A7_A9CD_7D306B38AE53
+#include<stdio.h>
+
+int main()
+{
+    printf("Curso de programacao\n");
+}
+
+#endif /* D43627EB_42A8_48A7_A9CD_7D306B38AE53 */
